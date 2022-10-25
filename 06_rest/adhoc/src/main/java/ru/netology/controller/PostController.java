f47b0939335524195018a0fa4ba6216147b61be1
Tural Guluev <tural-98@bk.ru>
2022-10-25 23:15:59 +0400

    the aplication is working, but a m not using RequestMapping"

--- 06_rest/adhoc/src/main/java/ru/netology/controller/PostController.java
+++ 06_rest/adhoc/src/main/java/ru/netology/controller/PostController.java
@@ -3,6 +3,7 @@ package ru.netology.controller;
 import com.google.gson.Gson;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 import ru.netology.service.PostService;
 
@@ -10,40 +11,57 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Controller
+@RestController
 @RequestMapping("/api/posts")
 public class PostController {
-  public static final String APPLICATION_JSON = "application/json";
-  private final PostService service;
+    public static final String APPLICATION_JSON = "application/json";
+    private final PostService service;
 
-  public PostController(PostService service) {
-    this.service = service;
-  }
+    public PostController(PostService service) {
+        this.service = service;
+    }
 
-  @GetMapping
-  public void all(HttpServletResponse response) throws IOException {
-    response.setContentType(APPLICATION_JSON);
-    final var data = service.all();
-    final var gson = new Gson();
-    response.getWriter().print(gson.toJson(data));
-  }
+    @GetMapping
+    public void all(HttpServletResponse response) throws IOException {
+        response.setContentType(APPLICATION_JSON);
+        final var data = service.all();
+        final var gson = new Gson();
+        response.getWriter().print(gson.toJson(data));
+    }
 
-  @GetMapping("/{id}")
-  public void getById(@PathVariable long id, HttpServletResponse response) {
-    // TODO: deserialize request & serialize response
-  }
+    @GetMapping("/{id}")
+    public void getById(@PathVariable long id, HttpServletResponse response) {
+        response.setContentType(APPLICATION_JSON);
+        try {
+            response.getWriter().print(service.getById(id));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NotFoundException e) {
+            try {
+                response.getWriter().print(String.format("user with %d id not found", id));
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
 
-  @PostMapping
-  public void save(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType(APPLICATION_JSON);
-    final var gson = new Gson();
-    final var post = gson.fromJson(request.getReader(), Post.class);
-    final var data = service.save(post);
-    response.getWriter().print(gson.toJson(data));
-  }
+    }
 
-  @DeleteMapping("/{id}")
-  public void removeById(long id, HttpServletResponse response) {
-    // TODO: deserialize request & serialize response
-  }
+    @PostMapping
+    public void save(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType(APPLICATION_JSON);
+        final var gson = new Gson();
+        final var post = gson.fromJson(request.getReader(), Post.class);
+        final var data = service.save(post);
+        response.getWriter().print(gson.toJson(data));
+    }
+
+    @DeleteMapping("/{id}")
+    public void removeById(@PathVariable long id, HttpServletResponse response) {
+        service.removeById(id);
+        try {
+            response.getWriter().write(String.format("The user with %d id has been deleted", id));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- 06_rest/adhoc/src/main/java/ru/netology/repository/PostRepositoryStubImpl.java
+++ 06_rest/adhoc/src/main/java/ru/netology/repository/PostRepositoryStubImpl.java
@@ -3,24 +3,43 @@ package ru.netology.repository;
 import org.springframework.stereotype.Repository;
 import ru.netology.model.Post;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Repository
 public class PostRepositoryStubImpl implements PostRepository {
+  private static AtomicInteger count = new AtomicInteger(0);
+  private final ConcurrentMap<Integer, Post> map=new ConcurrentHashMap<>();
+
+
   public List<Post> all() {
-    return Collections.emptyList();
+    return new ArrayList<>(map.values());
   }
 
   public Optional<Post> getById(long id) {
-    return Optional.empty();
+    return map.entrySet().stream().
+            filter(x -> x.getKey() == id).
+            map(Map.Entry::getValue).
+            findAny();
   }
 
   public Post save(Post post) {
+    if (post.getId() == 0) {
+      map.put(count.addAndGet(1), post);
+      post.setId(count.get());
+    } else {
+      Post mapPost = map.computeIfPresent((int) post.getId(), (key, value) -> post);
+      if (mapPost == null) {
+        System.out.println("Ошибка обновления данных. ");
+        return null;
+      }
+    }
     return post;
   }
 
   public void removeById(long id) {
+    map.remove((int) id);
   }
 }
